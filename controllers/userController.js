bdbb927311721435447cafca126a67d7b76d553f
xeniakadar <xenia.kadar@gmail.com>
2023-08-09 18:18:41 +0200

    fixed signup and added jwt login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,111 @@
+/* eslint-disable */
 const { body, validationResult } = require("express-validator");
+
 const asyncHandler = require("express-async-handler");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
-
-const User = require("../controllers/user");
+const express = require('express');
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 // create user
-exports.signup_get = (req, res, next) => {
-  res.render()
-}
+// exports.signup_post = async (req, res) => {
+//   const user = new User({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password,
+//   });
+//   try {
+//     const newUser = await user.save();
+//     res.status(201).json({ message: `user registered successfully: ${newUser}`});
+//   } catch (error) {
+//     res.status(500).json({ error: "error registering user"});
+//   }
+// };
+
+exports.signup_post = [
+  body("username", "Username must be specified")
+    .custom(async(username) => {
+      try {
+        const usernameExists = await User.findOne({ username: username });
+        if (usernameExists) {
+          throw new Error('Username already exists');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    })
+    .trim()
+    .escape(),
+  body("email")
+    .trim()
+    .escape()
+    .isEmail()
+    .withMessage("Email must be specified"),
+  body("password", "Password must be specified")
+    .trim()
+    .escape(),
+
+  async(req, res) => {
+    const errors = validationResult(req);
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(req.body.password, salt);
+    if (!errors.isEmpty()) {
+      return res.status(403).json({
+        username: req.body.username,
+        errors: errors.array(),
+      });
+    }
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: passwordHash,
+    });
+    try {
+      await user.save();
+      return res.status(201).json({
+        message: "User registered successfully",
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+        }
+      });
+    } catch(error) {
+      return res.status(500).json({ error: "error registering user"});
+    }
+  }
+];
+
+exports.login_post = async function (req, res, next) {
+  try {
+    passport.authenticate('local', { session: false}, (err, user, info) => {
+      if (err || !user) {
+        const error = new Error('User does not exist');
+        return res.status(403).json({
+          info
+        })
+      }
+      req.login(user, {session: false}, (err) => {
+        if (err) {
+          next(err);
+        }
+        // token
+        const body = {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+        };
+        const token = jwt.sign({ user: body}, process.env.SECRET, {expiresIn: '1d'});
+
+        return res.status(200).json({body, token});
+
+      });
+    }) (req, res, next);
+  } catch (err) {
+    res.status(403).json({
+      err
+    })
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,12 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const User = require("../models/user");
 // const blogpost_controller = require("../controllers/blogpostController");
+const user_controller = require("../controllers/userController");
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-
-router.get('/users', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
-router.post('/users', (req, res) => {
-  return res.send('Received a POst HTTP method');
-});
-router.put('/users/:userId', (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-});
-router.delete('/users/:userId', (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
 
 
 // user info
@@ -30,25 +18,21 @@ router.get('/', (req, res) => {
   return res.send("hello server");
 })
 
-router.post('/api/register', async(req, res) => {
-  const { username, email, password} = req.body;
-  try {
-    const user = await User.create({ username, email, password});
-    res.status(201).json({ message: `user registered successfully ${req.params.username}`});
-    console.log(req.body);
-  } catch (error) {
-    res.status(500).json({ error: "error registering user"});
-  }
-});
+router.post('/api/register', user_controller.signup_post);
 
-router.post('/api/login', passport.authenticate('local'), (req, res) => {
-  res.json({ message: 'logged in successfully', user: req.user});
-});
+// router.post('/api/login', passport.authenticate('local'), (req, res) => {
+//   res.json({ message: 'logged in successfully', user: req.user});
+// });
+router.post('/api/login', user_controller.login_post);
+
+
+
 
 router.post('/api/logout', (req, res) => {
   req.logout();
   res.json({ message: 'logged out succesffuly'});
 });
+
 
 
 module.exports = router;
