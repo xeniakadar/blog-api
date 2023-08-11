ffb1adaef2e0e9a8bf65396b943466e2c4991149
xeniakadar <xenia.kadar@gmail.com>
2023-08-11 19:51:13 +0200

    added functionality to adding comments to blogposts

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,10 +5,28 @@ const bodyParser = require("body-parser");
 const Comment = require("../models/comment");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const Blogpost = require("../models/blogpost");
 
-const {body, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-exports.comment_create = [
+exports.comment_create = [(req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    jwt.verify(req.token, process.env.SECRET, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        req.authData = authData;
+        next();
+      }
+    })
+  } else {
+    res.sendStatus(403);
+  }
+},
   body("text", "Comment must be at least 2 characters long")
     .trim()
     .isLength({ min: 2 })
@@ -16,20 +34,25 @@ exports.comment_create = [
 
   async(req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(403).json({
         text: req.body.text,
         errors: errors.array(),
       })
     }
+
     const comment = new Comment({
       text: req.body.text,
       timestamp: new Date(),
-      username: req.user.username,
-
+      username: req.authData.user.username,
     });
+
     try {
       await comment.save();
+      const blogpost = await Blogpost.findById(req.params.id);
+      blogpost.comments.push(comment._id);
+      await blogpost.save();
       return res.status(201).json({
         message: "comment successfully published",
         comment: {
@@ -40,6 +63,7 @@ exports.comment_create = [
         }
       });
     } catch(error) {
+      console.log("error creating message", error);
       return res.status(500).json({ error: "Error creating comment"});
     }
   }
