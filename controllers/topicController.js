3a9eb6db5e3b804553b34ab200c7399d8ab083d2
xeniakadar <xenia.kadar@gmail.com>
2023-08-22 20:28:44 +0200

    added topic populate

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -72,7 +72,7 @@ exports.topic_detail = async (req, res, next) => {
   try {
     const [topic, blogpostsInTopic ] = await Promise.all([
       Topic.findById(req.params.topicId).exec(),
-      Blogpost.find({ topic: req.params.topicId}, "title username").exec(),
+      Blogpost.find({ topic: req.params.topicId }, "title username").populate("title username timestamp text comments ").exec(),
     ]);
 
     if (!topic) {
