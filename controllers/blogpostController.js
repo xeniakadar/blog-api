fc6aa0cc9df877dc653a9e8cb3ce6fd2ca374760
xeniakadar <xenia.kadar@gmail.com>
2023-09-10 18:01:34 +0200

    fix not getting username

--- controllers/blogpostController.js
+++ controllers/blogpostController.js
@@ -103,7 +103,7 @@ exports.blogpost_list = async (req, res) => {
 exports.blogpost_detail = async (req, res, next) => {
   try {
     const blogpost = await Blogpost.findById(req.params.id)
-    .populate("user topic comments")
+    .populate("user topic comments comments.user")
     .exec();
 
     if (blogpost === null) {
